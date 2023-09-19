8b402a7eee1059386bc469f49dac67d0de0a43bb
digesaremarco <digesaremarco@gmail.com>
2023-09-19 16:18:01 +0200

    Added set and get methods

--- Note.cpp
+++ Note.cpp
@@ -15,3 +15,19 @@ bool Note::operator==(const Note &other) {
 std::string Note::getTitle() const {
     return title;
 }
+
+void Note::setTitle(std::string t) {
+    title = t;
+}
+
+void Note::setModify(bool m) {
+    modify = m;
+}
+
+std::string Note::getText() const {
+    return text;
+}
+
+void Note::setText(std::string t) {
+    text = t;
+}
--- Note.h
+++ Note.h
@@ -18,8 +18,15 @@ public:
     Note(std::string ti, std::string te, bool m) : title(ti), text(te), modify(m) {}
 
     bool getModify() const;
+    void setModify(bool m);
 
     std::string getTitle() const;
+
+    void setTitle(std::string t);
+
+    std::string getText() const;
+
+    void setText(std::string t);
 
     bool operator==(const Note &other);
 
